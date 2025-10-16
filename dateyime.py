024ff79afdf93d5fb0d89ba7651f7b3590093a02
maryamcody <maryamabdulhakim2010@gmail.com>
2025-10-16 10:02:21 +0300

    Add files via upload

---
+++ dateyime.py
@@ -0,0 +1,7 @@
+# using now() to get current date and time
+import datetime
+
+current_time = datetime.datetime.now()
+print ("time in greenwich meridian is : ", current_time)
+import calendar
+print("\n ",calendar.calendar(2021))
---
+++ evenodd.py
@@ -0,0 +1,7 @@
+number= int(input("Enter a number:"))
+print("number to be checked is:",number)
+if number%2==0:
+    print("the number is even number")
+else:
+    print("the number is odd number")   
+    
