1bb0369b1fb1a7862fd5e8a666e49ec03645488a
maryamcody <maryamabdulhakim2010@gmail.com>
2025-10-17 10:46:07 +0300

    Add files via upload

--- Loops.py
+++ Loops.py
@@ -2,3 +2,18 @@ n= int(input("Enter a number:"))
 
 for i in range(1,11):
     print (f"{n}x{i}={n*i}")
+
+n=int(input('Enter number of rows:'))
+for j in range(1,n+1):
+    for k in range(j):
+        print('*' ,end=' ')
+    print()
+
+n=int(input("Enter a number:"))
+for i in range(2,n-1):
+    if n%i==0:
+        print(f"{n} is not a prime number")
+        break
+else:
+        print(f"{n} is a prime number") 
+     
