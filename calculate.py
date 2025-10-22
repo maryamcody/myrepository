819666ac902265762ff06ead16370a88751bfa91
maryamcody <maryamabdulhakim2010@gmail.com>
2025-10-22 10:11:09 +0300

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,16 @@
+def  add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))    
+
+print("sum :", add (num1,num2))
+print("difference :", subtract(num1,num2))
+print("product :",multiply(num1,num2))
+print("division :",divide(num1,num2))
