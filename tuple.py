ea5efcc7195849ed94fb5081a0bb148d52651f4b
maryamcody <maryamabdulhakim2010@gmail.com>
2025-10-27 10:28:27 +0300

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+my_tuple=()
+print(type(my_tuple)) 
+
+my_tuple=(1,2,3,4,5)
+print(my_tuple)
+
+my_tuple2=("maryam",[1,3,5],(4,5,6))
+print(my_tuple2[0][2])
+print(my_tuple2[1][1])
+print(my_tuple2[2][1])
+
+sliced_tuple=my_tuple2[1:3]
+print(sliced_tuple)
