702a6d710f5db44e60b38f917ba4a5299adb1c3c
maryamcody <maryamabdulhakim2010@gmail.com>
2025-10-27 10:40:55 +0300

    Add files via upload

---
+++ set.py
@@ -0,0 +1,6 @@
+my_set={1,2,2,3,3,3,4,4,4,4}
+my_set2={1,5,5,5,2,2,7,7,7}
+print(my_set,my_set2)
+print(my_set.union(my_set2))
+print(my_set.intersection(my_set2))
+print(my_set.difference(my_set2))
