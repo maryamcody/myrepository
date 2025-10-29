d90fb4d50cec54f52dc44da2a5b28812034b3ea7
maryamcody <maryamabdulhakim2010@gmail.com>
2025-10-29 13:30:01 +0300

    Add files via upload

--- object-2.py
+++ object-2.py
@@ -4,9 +4,16 @@ class parrot:
   def __init__(self,name,age):
     self.name=name
     self.age=age
+  def sing(self,song):
+    return "{} sings {}".format(self.name,song)
+  def dance(self,dance):
+    return "{} dances {}".format(self.name,dance)
+  
 blue=parrot("Bluie",10)
 Woo=parrot("Woo",15)
 print("{} is a {} and is {} years".format(blue.name,blue.species,blue.age))
 print("{} is a {} and is {} years".format(Woo.name,Woo.species,Woo.age))
+print(blue.sing("Happy"))
+print(Woo.dance("Hip Hop"))
 
  
