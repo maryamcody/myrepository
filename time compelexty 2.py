c257d43f90561f1cf10c72264e14132aeef47099
maryamcody <maryamabdulhakim2010@gmail.com>
2025-11-24 10:28:45 +0300

    Add files via upload

---
+++ time compelexty 2.py
@@ -0,0 +1,10 @@
+def ontime(n):
+    iteration = 0
+    for i in range(1, n + 1):
+        iteration+=1
+    print("Number entered by user is:", n)
+    print("Total number of iterations is:", iteration)
+ontime(10)
+ontime(20)
+ontime(42)
+print("\n with any 'n' the time will change")
