c35c292cea2e546c947f28d8f7528caefe18ba1b
maryamcody <maryamabdulhakim2010@gmail.com>
2025-12-04 10:27:47 +0300

    Add files via upload

---
+++ bit manipulation.py
@@ -0,0 +1,21 @@
+num1= 10
+num2= 4
+#Using AND operation
+print("num1 & num2 =", num1 & num2)
+
+#Using OR operation
+print ("num1| num2 =", num1 | num2)
+
+#using NOt operation
+print("\n~num1=", ~num1)
+
+#Using XOR operation
+print("\nnum1^ num2=", num1^num2)
+
+#Using Right Shift operation 
+print("\nnum1>>1 =", num1>>1)
+print("\nnum2>>1=", num2>>1)
+
+#Using Left Shift operation
+print("\nnum1<<1= ", num1<<1)
+print("\nnum2<<1= ", num2<<1)
