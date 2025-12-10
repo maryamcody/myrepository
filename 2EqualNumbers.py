3add34468cf86d28695a5f23676337851139686c
maryamcody <maryamabdulhakim2010@gmail.com>
2025-12-10 10:19:01 +0300

    Add files via upload

---
+++ 2EqualNumbers.py
@@ -0,0 +1,8 @@
+def checkifSame(num1,num2):
+    if (num1^ num2)==0 :
+        print("both number are equal " )
+    else:
+        print("The Number are not equal ")
+num1=int(input("Enter a number "))
+num2=int(input("Enter a number "))
+checkifSame(num1,num2)
